7ed3320d9010b942cbcd8406ccee77ade0990c0c
Soraiya11-7 <soraiyaparvin@gmail.com>
2024-12-02 22:31:49 +0600

    firebase authentication has been added

--- index.js
+++ index.js
@@ -5,15 +5,11 @@ require('dotenv').config();
 const app = express();
 const port = process.env.Port || 5000;
 
-
 //middleware
-
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_SECRET_KEY}@cluster0.uoi62.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -30,6 +26,7 @@ async function run() {
     await client.connect();
 
     const coffeeCollection = client.db("coffeeDB").collection("coffee");
+    const userCollection = client.db("coffeeDB").collection("users");
 
     app.get('/coffee', async (req, res) => {
       const cursor = coffeeCollection.find();
@@ -44,8 +41,6 @@ async function run() {
       const result = await coffeeCollection.findOne(query);
       res.send(result);
     });
-
-
 
     app.post('/coffee', async (req, res) => {
       const newCoffee = req.body;
@@ -82,6 +77,40 @@ async function run() {
     })
 
 
+    //users related.....
+
+    app.get('/users', async (req, res) => {
+      const cursor = userCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post('/users', async (req, res) => {
+      const newUser = req.body;
+      console.log(newUser);
+      const result = await userCollection.insertOne(newUser);
+      res.send(result);
+    });
+
+    app.patch('/users', async (req, res) => {
+      const email = req.body.email;
+      const filter = { email };
+      const updatedDoc = {
+        $set: {
+          lastSignInTime: req.body?.lastSignInTime
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedDoc);
+      res.send(result);
+    })
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -91,8 +120,6 @@ async function run() {
   }
 }
 run().catch(console.dir);
-
-
 
 //root
 app.get('/', (req, res) => {
